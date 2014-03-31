b451898007135817bfe0412842434a8cbfe3aef6
mingcute <bestdearlove@gmail.com>
2014-03-31 18:14:09 +0800

    - Initial commit.

--- animatePractice/ViewController.m
+++ animatePractice/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "JMDropMenuView.h"
 
-@interface ViewController ()
+@interface ViewController () <JMDropMenuViewDelegate>
 
 @end
 
@@ -18,6 +19,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *view1 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    view1.backgroundColor = [UIColor clearColor];
+    [view1 setImage:[UIImage imageNamed:@"camera.png"]];
+    [self.view addSubview:view1];
+    
+    UIImageView *view2 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    view2.backgroundColor = [UIColor clearColor];
+    [view2 setImage:[UIImage imageNamed:@"album.png"]];
+    [self.view addSubview:view2];
+    
+    UIImageView *view3 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, 100, 100)];
+    view3.backgroundColor = [UIColor clearColor];
+    [view3 setImage:[UIImage imageNamed:@"search.png"]];
+    [self.view addSubview:view3];
+    
+    JMDropMenuView *menu = [[JMDropMenuView alloc] initWithViews:@[view1, view2, view3]];
+    menu.delegate = self;
+    [self.view addSubview:menu];
+    [menu popOut];
+
+}
+
+- (void)dropMenu:(JMDropMenuView *)menu didSelectAtIndex:(NSInteger)index
+{
+    NSLog(@"index %ld", (long)index);
+    [menu dismiss];
 }
 
 - (void)didReceiveMemoryWarning
